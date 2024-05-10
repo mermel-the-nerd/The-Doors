1a733e065c0173cd47fbeadae5bfb3c6bf57df5b
Maddie <mermelsteinmaddie@gmail.com>
2024-05-10 08:59:32 -0700

    got fishcount transferred between html files!

--- branch1.js
+++ branch1.js
@@ -3,5 +3,8 @@ function openDoor(){
 }
 
 function nextDoor(){
-    
+
 }
+let info = document.getElementById("info");
+const urlParams = new URLSearchParams(window.location.search);
+  info.innerHTML = urlParams.get('fishCount');
