f0f389e11535679013d4ab8b5a41992777551882
sabrina cheung <sc26448@westridge.org>
2024-05-14 09:10:43 -0700

    riddle function is functioning now

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//eveyerhting in here is in the function!
-let answer = "ding-dong"
-let userguess = document.getElementById("input");
-let lose = "Incorrect."
-let winlose = document.getElementById("winlose")
-if (userguess === answer){
-  winlose.innerText = win
-  
-}
-else {
-  winlose.innerText = lose    
+function checkAnswer(){
+    let userguess = document.getElementById("input");
+    let winlose = document.getElementById("winlose");
+    if (userguess.value === "ding-dong"){
+      winlose.innerText = "Correct.";
+      document.getElementById("startButton").style.display = "block";
+    }
+    else {
+      winlose.innerText = "Incorrect.";
+    }
 }
