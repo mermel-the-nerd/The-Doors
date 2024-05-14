aa81303d98b14bb74699a9a7e2bddd542083a6c3
sabrina cheung <sc26448@westridge.org>
2024-05-14 09:50:56 -0700

    starting on for loop

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function checkAnswer(){
+for (i=0; i<3; i+0){
+    function checkAnswer(){
     let userguess = document.getElementById("input");
     let winlose = document.getElementById("winlose");
     if (userguess.value === "ding-dong" || userguess.value === "a ding-dong"){
@@ -7,5 +8,7 @@ function checkAnswer(){
     }
     else {
       winlose.innerText = "Incorrect.";
+      i++
     }
+  }
 }
