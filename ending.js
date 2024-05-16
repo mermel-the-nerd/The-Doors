974af8813e2cb00388f601b239c8143e67bebce0
Fiala Pejic <>
2024-05-16 09:53:56 -0700

    fixed ending and added img

--- ending.js
+++ ending.js
@@ -1,27 +1,30 @@
 const urlParams = new URLSearchParams(window.location.search);
 const fishCount = urlParams.get("fishCount");
-const fishDisplay = document.getElementById("fishDisplay");
-fishDisplay.innerHTML = ("Fish Count: " + fishCount);
-
+const fishDisplay = /*document.getElementById("fishDisplay");
+fishDisplay.innerHTML = ("Fish Count: " + fishCount);*/
+endDoor= document.getElementById("endDoor");
+endDoor.style.visibility = 'visible';
 results = document.getElementById("results");
+
 function end(){
-  if (fishCount < 1) {
+
+    if (fishCount < 1) {
     results.innerHTML = "Grade: F <br/> The cat ate you because you didn't bring any fish"
-    document.getElementById("winImg").src = "winImgF.jpeg";
+    endDoor.src = "winImgF.jpeg";
       //add img src
   }
   else if (fishCount < 4) {
     results.innerHTML = "Grade: C <br/> The cat begrudgingly eats your fish but is disappointed in your small offering"
-    document.getElementById("winImg").src = "winImgC.jpeg";
+    endDoor.src = "winImgC.jpeg";
       //add img src
   }
   else if (fishCount < 7) {
     results.innerHTML = "Grade: B <br/> The cat eats your fish happily"
-    document.getElementById("winImg").src = "winImgB.jpeg";
+    endDoor.src = "winImgB.jpeg";
       //add img src
   }
   else {
     results.innerHTML = "Grade: A++++++ <br/> The cat eats all your fish and is satisfied. Congrats on getting the highest possible score!"
-    document.getElementById("winImg").src = "winImgA.jpeg";
+    endDoor.src = "winImgA.jpeg";
   }
 }
