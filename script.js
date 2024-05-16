b55d81b44f1947b13fd84e13dbbcab7695cb9463
sabrina cheung <sc26448@westridge.org>
2024-05-16 09:20:31 -0700

    this js is evil and is not working (yet hopefully autumn come backk)

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-for (i=0; i<3; i+0){
+for (i=0; i<3; i++){
     function checkAnswer(){
     let userguess = document.getElementById("input");
     let winlose = document.getElementById("winlose");
+    let i=0;
     if (userguess.value === "ding-dong" || userguess.value === "a ding-dong"){
       winlose.innerText = "Correct.";
       document.getElementById("startButton").style.display = "block";
+      i--;
     }
+    else if (i>2){
+        winlose.innerText = "You have run out of chances. You cannot pass through the door.";
+      }
     else {
-      winlose.innerText = "Incorrect.";
-      i++
+        winlose.innerText = "Incorrect.";
+      }
     }
+    console.log (i);
   }
-}
+
