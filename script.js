9f1498c3d1eb666fa7a34639b13d493275144f4d
sabrina cheung <sc26448@westridge.org>
2024-05-16 09:49:44 -0700

    added a comment & js still doesnt work yippee

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-for (i=0; i<3; i++){
+//the for loop automatically runs, without user input
+for (i=0; i<4; i++){
     function checkAnswer(){
     let userguess = document.getElementById("input");
     let winlose = document.getElementById("winlose");
     let i=0;
-    if (userguess.value === "ding-dong" || userguess.value === "a ding-dong"){
+    if (userguess.value === "ding-dong" || userguess.value === "a ding-dong" || userguess.value === "ding dong" || userguess.value === "a ding dong"){
       winlose.innerText = "Correct.";
       document.getElementById("startButton").style.display = "block";
       i--;
@@ -11,7 +12,7 @@ for (i=0; i<3; i++){
     else if (i>2){
         winlose.innerText = "You have run out of chances. You cannot pass through the door.";
       }
-    else {
+    else{
         winlose.innerText = "Incorrect.";
       }
     }
